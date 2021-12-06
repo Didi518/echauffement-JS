2fe5d809777eeb603da602c1a6ad727fe858d338
Didi518 <94155950+Didi518@users.noreply.github.com>
2021-12-06 11:18:04 +0100

    Add files via upload

---
+++ echauffement-JS/echauffement.js
@@ -0,0 +1,16 @@
+//exo1
+var maVar;
+maVar = 0;
+if (maVar < 10) {
+    maVar++;
+    console.log(maVar)
+}
+//exo2
+var maVar;
+maVar = 0;
+var myVar;
+myVar = Math.floor(Math.random(1, 100));
+if ('maVar < 20') {
+    maVar * myVar;
+    console.log(result);
+}
