66405749a15bac9f55e22b3bcfd7b57204d292db
Didi518 <kevin.ouali@gmail.com>
2021-12-07 09:33:38 +0100

    salut

---
+++ tableaux.js
@@ -0,0 +1,7 @@
+//exo1
+let annee = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre'];
+console.log(annee);
+
+//exo2
+let annee = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre'];
+console.log(annee[2]);
